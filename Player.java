48093a98831809763a8e94583302bac6dd09db8a
Dinithimalsha <77944696+Dinithimalsha@users.noreply.github.com>
2021-07-07 17:34:40 +0530

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,2 +1,29 @@
 public class Player {
+    private String name;
+    private int age;
+    private String type;
+    private String statistics;
+  
+    public player (String name, int age, String type, String statistics){
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.statistics = statistics;
+    }
+  
+    public void getAverage(){
+    
+    }
+    
+    public void getPlayerDetails(){
+    
+    }
+  
+    public void getBestPerformance(){
+    
+    }
+  
+    public void getPlayerType(){
+    
+    }
 }
