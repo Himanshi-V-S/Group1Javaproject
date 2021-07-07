59fc3efdaa7c5e65af276f4d4ac5f210fb661af7
sachz1997 <87040899+sachz1997@users.noreply.github.com>
2021-07-07 18:11:34 +0530

    Update PlayerSelection.java

--- PlayerSelection.java
+++ PlayerSelection.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class PlayerSelection {
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
+        
+        Player player1 = new Player("Kusal Perera", 31, "batsman", 508);
+        Player player2 = new Player("Danushka Gunathilaka", 30, "batsman", 449);
+        Player player3 = new Player("Wanindu De Silva", 24, "bowler", 693);
+        Player player4 = new Player("Lakshan Sandakan", 30, "bowler", 610);
+        Player player5 = new Player("Dushmantha Chameera", 29, "bowler", 482);
+        Player player6 = new Player("Lasith Malinga", 38, "bowler", 478);
+        Player player7 = new Player("Isuru Udana", 33, "bowler", 404);
+        
+        ArrayList<String> players = new ArrayList<String>();
+        
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+        players.add(player4);
+        players.add(player5);
+        players.add(player6);
+        players.add(player7);
 
         String menuChoice = "S";
 
